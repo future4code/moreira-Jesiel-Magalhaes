51c890eedc4501b6db6ae5c4aee06ba2bc054658
Jesiel Magalhães <91489057+JesielRM@users.noreply.github.com>
2021-11-30 15:48:51 -0300

    Exercício-Operadores

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,57 @@
+// EXERCÍCIOS DE INTERPRETAÇAO DE CÓDIGO
+
+//Exercício 1.
+//a. false
+//b. false
+//c. false
+//d. number
+
+//Exercício 2.
+//Da forma como o código está escrito, vai concatenar as strings por causa do prompt ao invés de somar que era o objetivo.
+
+//Exercício 3.
+//Para solucionar a questão deve adicionar Number na frente de prompt para converter a string em number e de fato o código poder somar.
+
+//EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+//Exercício 1.
+/* const idade = Number(prompt('Qual a sua idade'))
+const idadeAmigo = Number(prompt('Qual idade do seu amigo'))
+
+console.log(
+  'Sua idade é maior do que seu amigo?',
+  idade > idadeAmigo,
+  'Diferença de idade:',
+  idade - idadeAmigo
+) */
+
+//Exercício 2.
+/* const numero = Number(prompt('Escreva um número par'))
+resultado = numero % 2
+console.log('O resto da divisão é:', resultado) */
+//c. notei que sempre que digito um número par o padrão retornado no console é 0 (zero).
+
+//d. notei que sempre que digito um número impar o padrão retornado no console é 1 (um)
+
+//Exercício 3.
+/* let quantosAnos = Number(prompt('Quantos anos você tem?'))
+idadeEmMeses = quantosAnos * 12
+idadeEmDias = idadeEmMeses * 30
+idadeEmHoras = idadeEmDias * 24
+console.log('Sua idade em meses:', idadeEmMeses)
+console.log('Sua idade em dias:', idadeEmDias)
+console.log('Sua idade em horas:', idadeEmHoras) */
+
+//Exercício 4.
+
+/* let num1 = Number(prompt('Escreva um número'))
+
+let num2 = Number(prompt('Escreva um número'))
+
+console.log('O primeiro número é maior que o segundo?:', num1 > num2)
+
+console.log('O primeiro número é igual ao segundo?:', num1 === num2)
+
+console.log('O primeiro número é divisivel pelo segundo?:', num1 == num2)
+
+console.log('O segundo número é divisível pelo primeiro?:', num2 == num1) */
