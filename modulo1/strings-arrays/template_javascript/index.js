02d3cd3a5aee3777adcc682dda7ca26e2844f4b6
Jesiel Magalhães <91489057+JesielRM@users.noreply.github.com>
2021-12-01 17:02:36 -0300

    Exercício-de-Strings-e-Arrays

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,60 @@
+//   EXERCÍCIOS DE INTERPRETAÇÃO
+//1.
+//a. undefined
+//b. null
+//c. 11
+//d. 3
+//e. (11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//f. 9
+
+//2.SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+//EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+//1.
+/* let emailDoUsuario = prompt('Escreva seu e-mail')
+let nomeDoUsuario = prompt('Escreva seu nome')
+const fraseTemplate = `O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem vindo, ${nomeDoUsuario}`
+console.log(fraseTemplate) */
+
+//2
+//a.
+/*
+let comidasPreferidas = [
+  'Lasanha',
+  'Peixe',
+  'strogonoff',
+  'Batata Frita',
+  'Empadão'
+]
+console.log(comidasPreferidas)
+
+//b
+
+console.log(
+  `Essas são minhas comidas preferidas:
+  ${comidasPreferidas[0]}, 
+  ${comidasPreferidas[1]}, 
+  ${comidasPreferidas[2]}, 
+  ${comidasPreferidas[3]}, 
+  ${comidasPreferidas[4]}`
+)
+
+//c.
+let SuaComidaUsuario = prompt('Qual sua comida favorita')
+comidasPreferidas[1] = SuaComidaUsuario
+console.log(comidasPreferidas)
+
+
+
+//3.
+
+/* let listaDeTarefas = []
+let tarefa1 = prompt('Liste uma tarefa que você executa no seu dia?')
+let tarefa2 = prompt('Liste uma tarefa que você executa no seu dia')
+let tarefa3 = prompt('Liste uma tarefa que você executa no seu dia')
+listaDeTarefas.push(tarefa1, tarefa2, tarefa3)
+console.log(listaDeTarefas)
+let qualIndice = prompt('Qual indice você já realizou?')
+listaDeTarefas.splice(qualIndice, 1)
+console.log(listaDeTarefas) */
